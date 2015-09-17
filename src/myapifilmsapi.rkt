845fe685bc94cef0568695a9cda68eb139d8e97f
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-09-17 09:59:34 +0300

    Generalize get-filmography to not be only for actor
    Default 'type' is "Actor", but can be changed to any type ("Producer",
    "Director", etc.) to allow for searches beyond actors.

--- src/myapifilmsapi.rkt
+++ src/myapifilmsapi.rkt
@@ -10,11 +10,11 @@
          
          "movie.rkt")
 
-(define my-api-film-actor-url
+(define my-api-film-url
   "http://www.myapifilms.com/name?name=~a&filmography=1&format=JSON")
 
 (provide get-filmography)
-(define (get-filmography actor)
+(define (get-filmography person [type "Actor"])
   (define (year->number y)
     ;; The API prepends a space for the year, for some reason. Using
     ;; `with-matches` to extract the year in a reliable way
@@ -24,14 +24,14 @@
       #px"(\\d{4})" y
       (string->number (m 1))))
 
-  (match (hash-ref (car (call/input-url (string->url (format my-api-film-actor-url
-                                                             actor))
+  (match (hash-ref (car (call/input-url (string->url (format my-api-film-url
+                                                             person))
                                         get-pure-port
                                         read-json))
                    'filmographies)
     [(list fs ... a ofs ...)
      #:when (equal? (hash-ref a 'section)
-                    "Actor")
+                    type)
      (for/list ([m (hash-ref a 'filmography)])
        (movie (hash-ref m 'title)
               (year->number (hash-ref m 'year))))]))
