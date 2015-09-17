2d8062db1710239c46cca45bd8e12e55021de066
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-09-17 08:50:41 +0300

    Movied movie->file to movie.rkt for better fit
    Is now general for all applications, not just the De Niro case.

--- src/movie.rkt
+++ src/movie.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 
-(require racket/match)
+(require racket/match
+
+         "../src/omdbapi.rkt")
 
 (provide (struct-out movie))
 (struct movie (title year)
@@ -11,3 +13,17 @@
   (match m
     [`(movie ,title ,year)
       (movie title year)]))
+
+(provide movie->file)
+(define (movie->file m)
+  (call-with-output-file
+    (string-downcase (format "~a_~a.movie"
+                             (movie-year m)
+                             (regexp-replace* " "
+                                              (movie-title m)
+                                              "-")))
+    (lambda (op)
+      (write (movie/title-year->json (movie-title m)
+                                     (movie-year m))
+             op))
+    #:exists 'replace))
