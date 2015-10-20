efd6bf1340962651e4fb99295beaf31653b2b3e6
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-21 00:47:37 +0300

    Add `cache-path` function for re-use for cache-path

--- src/api/myapifilmsapi.rkt
+++ src/api/myapifilmsapi.rkt
@@ -35,37 +35,39 @@
         (string->number (m 1)))
       -1))
 
-  (when (and (in-cache? person type)
-             cache?)
-    (get-filmography/cached person #:type type))
+  (if (and (in-cache? person type)
+           cache?)
+    (get-filmography/cached person #:type type)
+    (let ([js-data (person->jsexpr person #:type type)])
+      (write-cache person type js-data)
 
-  (define js-data (person->jsexpr person #:type type))
-  (write-cache person type js-data)
-
-  (match (hash-ref (car js-data)
-                   'filmographies)
-    [(list fs ... a ofs ...)
-     #:when (equal? (hash-ref a 'section)
-                    type)
-     (for/list ([m (hash-ref a 'filmography)])
-       (movie (hash-ref m 'title)
-              (year->number (hash-ref m 'year))))]))
+      (match
+        (hash-ref (car js-data)
+                  'filmographies)
+        [(list fs ... a ofs ...)
+         #:when (equal? (hash-ref a 'section)
+                        type)
+         (for/list ([m (hash-ref a 'filmography)])
+           (movie (hash-ref m 'title)
+                  (year->number (hash-ref m 'year))))]))))
 
 (define (sanitize-name name)
   (regexp-replace #px"[ ']" (string-downcase name) ""))
 
+(define (cache-path person type)
+  (build-path 'up
+              "cache"
+              (format "~a_~a.cache"
+                      type
+                      (sanitize-name person))))
+
 (provide in-cache?)
 (define (in-cache? person type)
-  (file-exists? (build-path "cache"
-                            (format "~a_~a.cache"
-                                    type
-                                    (sanitize-name person)))))
+  (file-exists? (cache-path person type)))
 
 (provide write-cache)
 (define (write-cache person type filmography)
-  (call-with-output-file (build-path "cache"
-                                     (format "~a_~a.cache"
-                                             type (sanitize-name person)))
+  (call-with-output-file (cache-path person type)
                          (lambda (output-port)
                            (write filmography output-port))
                          #:exists 'replace))
@@ -86,10 +88,7 @@
 
   (match
     (hash-ref (car (call-with-input-file
-                     (build-path "cache"
-                                 (format "~a_~a.cache"
-                                         type
-                                         (sanitize-name person)))
+                     (cache-path person type)
                      read))
            'filmographies)
     [(list fs ... a ofs ...)
